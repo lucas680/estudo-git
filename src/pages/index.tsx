87837421be00f74c52acc437c60ee3eefba55b18
Lucas Pereira <74828159+lucas680@users.noreply.github.com>
2025-02-05 09:26:23 -0300

    removendo estilos padrão branch-teste2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div>
       <h1>Minha branch teste</h1>
-      <p>okok minha home</p>
+      <p>okok minha home description teste-2</p>
 
       <br /><br /><button>Clique aqui</button>
     </div>
